cb1196346b33c546f35bbca03d4707e861d18e09
Leevi Kerkelä <leevi.kerkela@protonmail.com>
2023-12-04 21:02:18 +0000

    Add raw suffix

--- fetch_raw_data.py
+++ fetch_raw_data.py
@@ -3,8 +3,8 @@ import requests
 
 
 if __name__ == "__main__":
-    if not os.path.exists("2-shell_HARDI"):
-        os.mkdir("2-shell_HARDI")
+    if not os.path.exists("2-shell_HARDI_raw"):
+        os.mkdir("2-shell_HARDI_raw")
 
     for fname in [
         "dwi.bval",
@@ -16,11 +16,11 @@ if __name__ == "__main__":
     ]:
         print(f"Downloading {fname}", end="\r")
         response = requests.get(
-            f"https://filedn.eu/lnoC7HTxOBmbFVGPA5CvIF4/2-shell_HARDI/{fname}"
+            f"https://filedn.eu/lnoC7HTxOBmbFVGPA5CvIF4/2-shell_HARDI_raw/{fname}"
         )
         if response.status_code == 200:
-            with open(os.path.join("2-shell_HARDI", fname), "wb") as file:
+            with open(os.path.join("2-shell_HARDI_raw", fname), "wb") as file:
                 file.write(response.content)
         else:
             print(f"Failed to download the file: {response.status_code}")
-    print("Finished downloading the dataset")
+    print("Finished downloading the data")
